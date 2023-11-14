4f8e6582a61b161c6d5b79cc405b1098561ce57b
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-11-14 17:01:05 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, jsx }"
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
