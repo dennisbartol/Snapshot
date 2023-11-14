f61edde96006a7cc81cddbfd7e0f5d15d4a17765
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-11-14 17:01:51 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+   import Camera from './components/Camera.vue';
+</script>
+
+<template>
+    <div class="min-h-screen flex items-center justify-center">
+        <Camera />
+
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+
+
+
